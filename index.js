a4c09a70f6f811338d6c232ed0714eede26efad0
Kelvin <kmwendwa476@gmail.com>
2022-06-13 13:11:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+//Declare a variable in global scope called customerName
+var customerName = "bob"
+
+const leastFavoriteCustomer = "Morio"
+
+//Write a function that accesses that global 
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+console.log(customerName)
+
+//Write a function that when called, declares a variable called bestCustomer in global scope and assigns it to be 'not bob'
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+//See the consequences of declaring a variable in global scope, by writing a new function called overwriteBestCustomer() that changes that bestCustomer variable.
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Mzae"
+} 
+
+console.log(leastFavoriteCustomer)
